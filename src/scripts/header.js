28bce8a550ff35f10df6c7d9214b27d3eceb671c
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-03 13:02:50 -0300

    iconos derecha

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,27 +1,20 @@
 import "../styles/header-style.css";
 import React from "react";
-import { FaSearch, FaShoppingBag} from "react-icons/fa";
+import { FaSearch, FaShoppingBag } from "react-icons/fa";
 
 function Header() {
   return (
-
     <header className="custom-header">
-
       <div className="floating-header"> 
         <button>holaa</button>
         <button>miaw</button>
         <button>miaw</button>
         <button>miaw</button>
       </div>
-
       <div className="header-right">
-    
-    {/* huh */}
-        <FaSearch className="icon" /> 
-    {/* Icono CARRITO */}
+        <FaSearch className="icon" />
         <FaShoppingBag className="icon" />
       </div>
-
     </header>
   );
 }
