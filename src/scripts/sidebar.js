e9a600b8cb648f98c3295357d94a1fc973c807bc
ThatNaomi <oprahmicroondas@gmail.com>
2025-04-04 11:49:06 -0300

    sidebar y header completos visualmente, cambio nombre de header-style.css a header.css

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,24 +1,25 @@
 import "../styles/sidebar.css";
 import React from "react";
-import { FaHome, FaBell, FaPlus, FaCommentDots, FaCog } from "react-icons/fa";
+//import { FaHome, FaBell, FaPlus, FaCommentDots, FaCog } from "react-icons/fa";
+
 
 function SideBar () {
     return (
 
-        <sidebar className = "custon-sidebar">
+        <aside className = "custom-sidebar">
 
-        <div className = "sidebar">
-        <button><FaHome /></button>
-                <button><FaPlus /></button>
-                <button><FaBell /></button>
-                <button><FaCommentDots /></button>
-                <button><FaCog /></button>
-        </div>
-
-
+<div className="sidebar">
+<button title="Inicio"><i className='bx bx-home'></i></button>
+<button title="Agregar"><i className='bx bx-plus'></i></button>
+<button title="Notificaciones"><i className='bx bx-bell'></i></button>
+<button title="Mensajes"><i className='bx bx-message'></i></button>
+<button title="Configuración"><i className='bx bx-cog'></i></button>
+</div>
 
 
-        </sidebar>
+
+
+        </aside>
 
     )
 }
