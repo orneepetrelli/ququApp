f4c2abed1fb2b1934aaa3855957a7faf3a51ed61
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-04 20:38:35 -0300

    saco registro

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'boxicons/css/boxicons.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Inicio from "./scripts/inicio";
 import Login from "./scripts/login";
-import Registro from "./scripts/registrarse";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Inicio/>}/>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/registro" element={<Registro/>}/>
       </Routes>
     </Router>
   );
