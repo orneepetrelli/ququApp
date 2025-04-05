b925b1d8cc6e881cc05c80c7e8607cef45219e8a
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-04 22:35:41 -0300

    .

--- src/scripts/tarjetasProductos.js
+++ src/scripts/tarjetasProductos.js
@@ -1,9 +1,32 @@
 import React from "react";
+import chanel from "../img/chanel.jpeg";
+import makeup from "../img/makeup.jpeg";
+import uñas from "../img/uñas.jpeg";
+import zapas from "../img/zapas.jpeg";
+
 
 function TarjetasProductos(){
+
+
+      const items = [
+        { image: chanel, title: "chanel"},
+        { image: makeup, title: "makeup"},
+        { image: uñas, title: "Uñas"},
+        { image: zapas, title: "zapatillas"},
+      ];
+
     return(
         <div className="container-tar-Products">
-            <h1>HOLAAAA</h1>
+            <div className="container-products">
+                <div className="container-img">
+                    <img src={items.image} alt={items.title}/>
+                    <span className="discount">-13%</span>
+                    <div className="button-group">
+                        
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
