04b7468c7a7236b91aba2caaac12bd35e89e4986
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-05 16:21:14 -0300

    card de producto

--- src/scripts/productos.js
+++ src/scripts/productos.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Header from "./header";
 import Footer from "./footer";
-import SideBar from "./sidebar";
 import TarjetasProductos from "./tarjetasProductos";
 
 function Productos(){
     return(
         <div>
-            <Header/>
-            <SideBar/>
             <div>
             <h1>Hola soy Productos</h1>
                 <TarjetasProductos/>
--- src/scripts/tarjetasProductos.js
+++ src/scripts/tarjetasProductos.js
@@ -3,32 +3,129 @@ import chanel from "../img/chanel.jpeg";
 import makeup from "../img/makeup.jpeg";
 import uñas from "../img/uñas.jpeg";
 import zapas from "../img/zapas.jpeg";
+import "../styles/tarjetasProductos.css";
 
-
-function TarjetasProductos(){
-
-
-      const items = [
-        { image: chanel, title: "chanel"},
-        { image: makeup, title: "makeup"},
-        { image: uñas, title: "Uñas"},
-        { image: zapas, title: "zapatillas"},
-      ];
-
-    return(
-        <div className="container-tar-Products">
-            <div className="container-products">
+function TarjetasProductos() {
+    return (
+        <div className="container-products">
+            <div className="card-product">
                 <div className="container-img">
-                    <img src={items.image} alt={items.title}/>
+                    <img src={chanel} alt="traje chanel" />
                     <span className="discount">-13%</span>
                     <div className="button-group">
-                        
+                        <span><i className="fa-regular fa-eye"></i></span>
+                        <span><i className="fa-regular fa-heart"></i></span>
+                        <span><i className="fa-solid fa-code-compare"></i></span>
                     </div>
                 </div>
+                <div class="content-card-product-wrapper">
+                <div className="content-card-product">
+                    <div className="stars">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
+                    </div>
+                    <h3>Traje Chanel</h3>
+                    <span className="add-cart"><i className="fa-solid fa-basket-shopping"></i></span>
+                    <p className="price">$4.60 <span>$5.30</span></p>
+                </div>
+                {/* <div className="extra-info">
+                    Opciones de pago: 3 cuotas sin interés 🔥
+                </div> */}
+            </div>
+            </div>
 
+            <div className="card-product">
+                <div className="container-img">
+                    <img src={makeup} alt="maquillaje" />
+                    <span className="discount">-22%</span>
+                    <div className="button-group">
+                        <span><i className="fa-regular fa-eye"></i></span>
+                        <span><i className="fa-regular fa-heart"></i></span>
+                        <span><i className="fa-solid fa-code-compare"></i></span>
+                    </div>
+                </div>
+                <div class="content-card-product-wrapper">
+                <div className="content-card-product">
+                    <div className="stars">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
+                    </div>
+                    <h3>Maquillaje</h3>
+                    <span className="add-cart"><i className="fa-solid fa-basket-shopping"></i></span>
+                    <p className="price">$5.70 <span>$7.30</span></p>
+                </div>
+                {/* <div className="extra-info">
+                    Opciones de pago: 3 cuotas sin interés 🔥
+                </div> */}
+                </div>
+            </div>
+
+            <div className="card-product">
+                <div className="container-img">
+                    <img src={uñas} alt="Uñas" />
+                    <span className="discount">-30%</span>
+                    <div className="button-group">
+                        <span><i className="fa-regular fa-eye"></i></span>
+                        <span><i className="fa-regular fa-heart"></i></span>
+                        <span><i className="fa-solid fa-code-compare"></i></span>
+                    </div>
+                </div>
+                <div class="content-card-product-wrapper">
+                <div className="content-card-product">
+                    <div className="stars">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                    </div>
+                    <h3>Uñas</h3>
+                    <span className="add-cart"><i className="fa-solid fa-basket-shopping"></i></span>
+                    <p className="price">$3.85 <span>$5.50</span></p>
+                </div>
+                {/* <div className="extra-info">
+                    Opciones de pago: 3 cuotas sin interés 🔥
+                </div> */}
+                </div>
+            </div>
+
+            <div className="card-product">
+                <div className="container-img">
+                    <img src={zapas} alt="zapatillas" />
+                    <div className="button-group">
+                        <span><i className="fa-regular fa-eye"></i></span>
+                        <span><i className="fa-regular fa-heart"></i></span>
+                        <span><i className="fa-solid fa-code-compare"></i></span>
+                    </div>
+                </div>
+                <div class="content-card-product-wrapper">
+                <div className="content-card-product">
+                    <div className="stars">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
+                    </div>
+                    <h3>Zapatillas</h3>
+                    <span className="add-cart"><i className="fa-solid fa-basket-shopping"></i></span>
+                    <p className="price">$5.60</p>
+                </div>
+                {/* <div className="extra-info">
+                    Opciones de pago: 3 cuotas sin interés 🔥
+                </div> */}
+            </div>
             </div>
         </div>
+
     );
 }
 
 export default TarjetasProductos;
+
