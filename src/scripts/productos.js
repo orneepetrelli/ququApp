dd1134706989f7e848bd50d1e602985e479f04c6
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-04 21:58:57 -0300

    Preparacion de procutos

--- src/scripts/productos.js
+++ src/scripts/productos.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import SideBar from "./sidebar";
+import TarjetasProductos from "./tarjetasProductos";
 
 function Productos(){
     return(
@@ -9,7 +10,8 @@ function Productos(){
             <Header/>
             <SideBar/>
             <div>
-                <h1>Hola soy Productos</h1>
+            <h1>Hola soy Productos</h1>
+                <TarjetasProductos/>
             </div>
             <Footer/>
         </div>
