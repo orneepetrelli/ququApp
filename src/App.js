91b09b1ba922d3c58fe668b684520dc82c216c47
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-06 13:40:49 -0300

    AGREGAR AL CARRITOOO

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/productos" element={<Productos agregarAlCarrito={agregarAlCarrito} />} />
 
-        <Route path="/informacionProducto" element={<InfoProducto />} />
+        <Route path="/informacionProducto" element={<InfoProducto agregarAlCarrito={agregarAlCarrito}/>} />
         {/* <Route path="/carrito" element={<Carrito />} /> */}
       </Routes>
     </Router>
