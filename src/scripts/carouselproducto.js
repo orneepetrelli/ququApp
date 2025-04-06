ec0b34221043b735e5f271ed90c8c195427cf066
ThatNaomi <oprahmicroondas@gmail.com>
2025-04-06 13:31:01 -0300

    borre infoproducto e hice uno mejorado sin css

--- src/scripts/carouselproducto.js
+++ src/scripts/carouselproducto.js
@@ -3,53 +3,7 @@ import '../styles/infoproducto.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CarouselProducto({ imagenes }) {
-  return (
-    <div
-      id="carouselProducto"
-      className="carousel slide carousel-fade"
-      data-bs-ride="carousel"
-      style={{ backgroundColor: "#fff", borderRadius: "20px" }} // fondo blanco para evitar flashes
-    >
-      <div className="carousel-inner">
-        {imagenes.map((img, index) => (
-          <div
-            className={`carousel-item ${index === 0 ? "active" : ""}`}
-            key={index}
-          >
-            <img
-  src={img}
-  className="d-block w-100 carousel-img"
-  alt={`Imagen ${index + 1}`}
-  style={{
-    maxHeight: "600px",
-    objectFit: "contain",
-    objectPosition: "center",
-    backgroundColor: "white",
-    margin: "auto"
-  }}
-/>
 
-          </div>
-        ))}
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselProducto"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" />
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselProducto"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" />
-      </button>
-      </div>
-  );
 }
 
 export default CarouselProducto;
