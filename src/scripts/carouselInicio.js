919804abfa0aaf3b753fd758bbfaa60f22edcef8
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-06 18:00:17 -0300

    agrego carouselInicio

---
+++ src/scripts/carouselInicio.js
@@ -0,0 +1,37 @@
+import React from "react";
+import img1 from "../img/inicio_1.jpeg";
+import img2 from "../img/inicio_2.jpeg";
+import img3 from "../img/inicio_3.jpeg";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/carouselInicio.css";
+
+function CarouselInicio() {
+    return (
+        <div>
+        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src={img1} className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img2} className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src={img3} className="d-block w-100" alt="..."/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    );
+}
+
+export default CarouselInicio; 
