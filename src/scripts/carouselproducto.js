55370f796658ee8bf05eea196f6783f3df7b2023
ThatNaomi <oprahmicroondas@gmail.com>
2025-04-06 11:31:45 -0300

    bordes chotos

--- src/scripts/carouselproducto.js
+++ src/scripts/carouselproducto.js
@@ -1,10 +1,11 @@
 import React from "react";
 import '../styles/infoproducto.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+/*<div id="carouselProducto" className="carousel slide" data-bs-ride="carousel">*/
 function CarouselProducto({ imagenes }) {
   return (
-    <div id="carouselProducto" className="carousel slide" data-bs-ride="carousel">
+    <div id="carouselProducto" className="carousel slide carousel-fade" data-bs-ride="carousel">
+    
       <div className="carousel-inner">
         {imagenes.map((img, index) => (
           <div
@@ -12,15 +13,18 @@ function CarouselProducto({ imagenes }) {
             key={index}
           >
             <img
-              src={img}
-              className="d-block w-100"
-              alt={`Imagen ${index + 1}`}
-              style={{
-                height: "500px",
-                objectFit: "cover",
-                borderRadius: "20px",
-              }}
-            />
+  src={img}
+  className="d-block w-100"
+  alt={`Imagen ${index + 1}`}
+  style={{
+    height: "600px",          // Altura similar a la imagen subida
+    objectFit: "contain",     // 🔥 Contiene la imagen sin recortarla
+    objectPosition: "center", // Centrada
+    backgroundColor: "#fff",  // Fondo blanco para que no quede vacío si no llena todo
+    padding: "1rem",          // Un poco de aire si querés
+    borderRadius: "0px"       // Quitás bordes redondeados para que no se vean en el cambio
+  }}
+/>
           </div>
         ))}
       </div>
