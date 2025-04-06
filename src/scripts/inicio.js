421831c3263fd5b68f880e9ff2a805c648e727c2
ornella.petrelli <ornella.petrelli@gmail.com>
2025-04-06 18:00:03 -0300

    saco carousel de inicio

--- src/scripts/inicio.js
+++ src/scripts/inicio.js
@@ -2,10 +2,8 @@ import React from "react";
 import Tarjetas from "./tarjetas";
 import SideBar from "./sidebar";
 import Footer from "./footer";
+import CarouselInicio from "./carouselInicio.js";
 import '../styles/inicio.css';
-import img1 from "../img/inicio_1.jpeg";
-import img2 from "../img/inicio_2.jpeg";
-import img3 from "../img/inicio_3.jpeg";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,27 +12,7 @@ function Inicio() {
     return (
         <div>
         <SideBar/>
-        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={img1} className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img2} className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                    <img src={img3} className="d-block w-100" alt="..."/>
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
+        <CarouselInicio/>
         <Tarjetas/>
         <Footer/>
     </div>
